c83c8fbec261b23127f7b023d1c9e7834f710847
tung2711 <117241266+tunglm271@users.noreply.github.com>
2023-10-14 11:10:45 +0700

    add 2.5 2.6

---
+++ lab01/CalculateTwoNumbers.java
@@ -0,0 +1,22 @@
+package lab01;
+
+import javax.swing.*;
+
+public class CalculateTwoNumbers {
+    public static void main(String[] args) {
+        String strNum1, strNum2;
+
+        strNum1 = JOptionPane.showInputDialog(null,"Please enter the first number:",
+                "Enter the first number",JOptionPane.INFORMATION_MESSAGE);
+        strNum2 = JOptionPane.showInputDialog(null,"Please enter the second number:",
+                "Enter the second number",JOptionPane.INFORMATION_MESSAGE);
+        double num1 = Double.parseDouble(strNum1);
+        double num2 = Double.parseDouble(strNum2);
+
+        JOptionPane.showMessageDialog(null,"sum:"  + (num1+num2) +"\n"
+        + "difference: " + (num1 - num2) +"\n"
+        + "product: " + num1*num2 + "\n"
+        + "quotient: " + num1/num2);
+        System.exit(0);
+    }
+}
